8d1e7b6ed17ead6d6734405aef49365c4f7b3fea
yepes94 <esvasye@gmail.com>
2019-04-26 19:37:20 +0200

    Retoques

--- src/main/java/Scraper.java
+++ src/main/java/Scraper.java
@@ -3,8 +3,10 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import java.util.Scanner;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Map;
 
 public class Scraper {
@@ -14,7 +16,12 @@ public class Scraper {
 
         //Create a new client and log us in!
         //TODO: Put ur credentials here:
-        MitraClient client = new MitraClient("", "");
+        System.out.println("Nombre: ");
+        Scanner entradaEscaner = new Scanner(System.in);
+        String nombre = entradaEscaner.nextLine();
+        System.out.println("Password: ");
+        String pass = entradaEscaner.nextLine();
+        MitraClient client = new MitraClient(nombre, pass);
         client.login();
 
         //Let's scrape our messages, information behind a login.
@@ -25,8 +32,40 @@ public class Scraper {
 
         //For each message in messages, let's print out message and a new line.
 
+        ArrayList<String> listAsignatura = new ArrayList<>();
+        ArrayList<String> liststNombreGeneralNota= new ArrayList<>();
+        ArrayList<String> listNombreExamen = new ArrayList<>();
+        ArrayList<String> liststPorcentaje = new ArrayList<>();
+        ArrayList<String> listNota = new ArrayList<>();
+
         for (Element message : messages) {
-            System.out.println(message.text() + "\n");
+            if(!message.text().contains("-")){
+                listAsignatura.add(message.text());
+            }else{
+                //Control laboratori - P1 3.6% 7.9
+                //split1 = [control lab, p1 3.6% 7.9]
+                //split1[1].(" ") : [p1, 3.6%, 7.9]
+                String[] split_one = message.text().split("-",2);
+                String[] split_two = split_one[1].split(" ");
+
+                liststNombreGeneralNota.add(split_one[0]);
+                if (split_two.length == 3){
+                    listNombreExamen.add(split_two[0]);
+                    liststPorcentaje.add(split_two[1]);
+                    listNota.add(split_two[2]);
+                }else{
+                    //Todo: Mejorar el nombre del examen, puede mostrar un nombre raro.
+                    listNombreExamen.add(split_two[split_two.length - 3]);
+                    liststPorcentaje.add(split_two[split_two.length - 2]);
+                    listNota.add(split_two[split_two.length - 1]);
+                }
+            }
+            System.out.println(message.text());
         }
+        System.out.println(listAsignatura);
+        System.out.println(listNombreExamen);
+        System.out.println(liststNombreGeneralNota);
+        System.out.println(liststPorcentaje);
+        System.out.println(listNota);
     }
 }
